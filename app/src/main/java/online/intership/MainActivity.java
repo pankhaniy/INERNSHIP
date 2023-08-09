63cc23acdb0315c473e09f720fe79c20cd77c4b0
Nihar <niharikapankhaniya@gmail.com>
2023-08-09 12:05:08 +0530

    28 LOGIN UI changees

--- app/src/main/java/online/intership/MainActivity.java
+++ app/src/main/java/online/intership/MainActivity.java
@@ -3,12 +3,29 @@ package online.intership;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("login Successfully");
+                Toast.makeText(MainActivity.this,"login successfully",Toast.LENGTH_SHORT).show();
+                Snackbar.make(view,"login successfully",1000).show();
+            }
+        });
     }
 }
